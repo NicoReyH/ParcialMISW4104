923c91d77005478493b529dbd0291d6ee857a9a9
Nicolas Rey <nicolasreydh@gmail.com>
2023-04-27 11:49:36 -0500

    class vehicle implementation

--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehicle } from './vehicle';
 
 @Component({
   selector: 'app-vehicle',
   templateUrl: './vehicle.component.html',
-  styleUrls: ['./vehicle.component.css']
+  styleUrls: ['./vehicle.component.css'],
 })
 export class VehicleComponent implements OnInit {
+  courses: Vehicle[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
